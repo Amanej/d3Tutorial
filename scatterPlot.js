ddc380e5650dde51270db0475987038ce1e81edb
Aman Mender <aman@aman-mbp.c10452.xaas.no>
2017-03-21 15:15:29 +0100

    Finished scatter plot

--- scatterPlot.js
+++ scatterPlot.js
@@ -29,16 +29,23 @@
           // The first line returns the TMAX - max temperature value for the data object in the current iteration
           return Math.abs(d.TMAX) / 10})
           // Setting the circle to x position
-            // 
+            // Positioning the circle to random value, that is also offset from the padding and times the width of total SVG minus the padding
           .attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
+          // Same as cx but for the y-axis
+            // Basically we are not using the data value in this callback
           .attr('cy', function(d) {return Math.max(0 + padding, Math.random() * height - padding)})
+            // Setting the stroke style attribute to red
             .style('stroke', 'red')
+            // We are using the data to determine which color the dots should use
+              // We are using the year value(from the date method in pure JS) to determine if its 1974 or 1973
             .style('fill', function(d) {
                 year = d.DATE.charAt(3)
                 if (year === "3") {
+                    // Blue circles are from 1973
                     return "blue"
                 }
                 else {
+                    // Green circles are from 1974
                     return "green"
                 }
             });
